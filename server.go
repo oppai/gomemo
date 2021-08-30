89261e2651439262243f6fb890632fbdfb47ebef
oppai <hotsoup.h@gmail.com>
2021-08-31 07:06:24 +0900

    Remove badcode

--- server.go
+++ server.go
@@ -10,7 +10,6 @@ func main() {
 	e := echo.New()
 	e.Static("/", "static")
 	e.GET("/api", func(c echo.Context) error {
-		foo := "foo"
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
